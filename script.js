230ff8a17ed512c5fe1b38e5ef857dcd448afb89
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-08-28 15:47:21 -0500

    Establish initial 16x16 grid

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-const container = document.querySelector('#container');
 
-let i=0, n=16;
-
-container.innerHTML = 
-    `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
-    .repeat(n);
+/* Select our grid container */
+const container = document.getElementById('grid-container');
 
 
-const rows = document.querySelectorAll('.row');
+/* Create grid*/
 
-const cells = document.querySelectorAll('.cell');
+function makeGrid(rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
+    for (let s = 0; s < (rows * cols); s++){
+        let square = document.createElement('div');
+        square.innerText = (s + 1);
+        container.appendChild(square).className = "grid-item";
+    }
+}
 
-rows.forEach(row => {
-    cells.forEach(cell => {
-        cell.addEventListener('mouseover', e => {
-            e.target.style.background = 'black';
-        });
-    })
-})
-
-
-
-
-
+/* Establish initial 16 x 16 grid */
+makeGrid(16, 16);
 
 
 
